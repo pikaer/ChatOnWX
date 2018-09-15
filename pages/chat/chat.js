f25ca78dbc29fba1f6662e53c5471ebe63d6a77b
易林军 <1441457008@qq.com>
2018-09-16 02:17:57 +0800

    页面容器更换
    页面容器更换

--- pages/chat/chat.js
+++ pages/chat/chat.js
@@ -2,39 +2,99 @@
 //获取应用实例
 const app = getApp()
 
+var page = 1;
+var page_size = 20;
+
+var GetList = function (self) {
+  self.setData({
+    hidden: false
+  });
+  let hasuserInfo = app.globalData.userInfoAPI.value == undefined;
+  if (hasuserInfo) {
+    wx.request({
+      url: app.globalData.baseUrl + 'api/Chat/GetChatList',
+      method: "POST",
+      data: {
+        "Head": { "Token": "", "AppType": 0 },
+        "Content": { 
+          "UserId": 1,
+          "PageIndex": page,
+          "PageCount": page_size
+        }
+      },
+      header: { "Content-Type": "application/json" },
+      success: function (res) {
+        if (res.data.head.success) {
+          console.info("获取聊天列表成功！")
+          var list = self.data.chatList;
+          for (var i = 0; i < res.data.content.length; i++) {
+            list.push(res.data.content[i]);
+          }
+          self.setData({
+            chatList: list
+          });
+          page++;
+          self.setData({
+            hidden: true
+          });
+        }
+        else{
+          console.error("获取聊天列表失败！");
+        }
+      },
+      fail: function (res) { console.error("获取聊天列表失败！"); }
+    })
+  }
+  else {
+    console.info("获取用户信息失败！")
+  }
+}
+
 Page({
   data: {
-    chatList: {}
+    chatList: [],
+    hidden: true,
+    scrollTop: 0,
+    scrollHeight: 400
+  },
+
+  onLoad: function () {
+    let self = this;
+    wx.getSystemInfo({
+      success: function (res) {
+        console.info("当前设备屏幕高度："+res.windowHeight);
+        self.setData({
+          scrollHeight: res.windowHeight
+        });
+      }
+    });
   },
 
   onShow: function () {
-    this.getChatList()
+    GetList(this);
   },
 
-  //获取聊天列表
-  getChatList: function () {
-    let hasuserInfo = app.globalData.userInfoAPI.value == undefined;
-    if (hasuserInfo)
-    {
-      var self = this;
-      wx.request({
-        url: app.globalData.baseUrl + 'api/Chat/GetChatList',
-        method: "POST",
-        data: {
-          "Head": { "Token": "", "AppType": 0},
-          "Content": { "UserId": 1 }
-        },
-        header: { "Content-Type": "application/json" },
-        success: function (res) {
-          self.setData({ chatList: res.data.content })
-          console.info("获取聊天列表成功！")
-        },
-        fail: function (res) { console.error("获取聊天列表失败！"); }
-      })
-    }
-    else {
-      console.info("获取用户信息失败！")
-    }
+  //滚动到底部时加载数据
+  bindDownLoad: function () {
+    var that = this;
+    GetList(that);
+  },
+
+  scroll: function (event) {
+    console.info(event)
+    this.setData({
+      scrollTop: event.detail.scrollTop
+    });
+  },
+
+//滚动到顶部刷新
+  refresh: function (event) {
+    page = 1;
+    this.setData({
+      scrollTop: 0,
+      chatList: []
+    });
+   GetList(this)
   },
 
   //清除未读提示
