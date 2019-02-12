92de599a5981407acea01557f618191d7e8be166
易林军 <1441457008@qq.com>
2019-02-13 00:57:13 +0800

    基础资料完善

--- pages/userinfo/userinfo.js
+++ pages/userinfo/userinfo.js
@@ -3,7 +3,10 @@ const app = getApp()
 Page({
 
   data: {
-    date: '2016-09-01'
+    date: '2016-09-01',
+    homeRegion: ['广东省', '广州市', '海珠区'],
+    homeTownRegion: ['广东省', '广州市', '海珠区'],
+    customItem: '全部'
   },
 
   onPullDownRefresh: function () {
@@ -15,7 +18,15 @@ Page({
       date: e.detail.value
     })
   },
-
-
+  bindHomeRegionChange: function (e) {
+    this.setData({
+      homeRegion: e.detail.value
+    })
+  },
+  bindHomeTownRegionChange: function (e) {
+    this.setData({
+      homeTownRegion: e.detail.value
+    })
+  }
 })
 
