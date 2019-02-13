367f026628eb631dc7489a7276f79ecb4d255ea2
易林军 <1441457008@qq.com>
2019-02-14 00:45:12 +0800

    userinfo

--- pages/userinfo/userinfo.js
+++ pages/userinfo/userinfo.js
@@ -3,10 +3,12 @@ const app = getApp()
 Page({
 
   data: {
-    date: '2016-09-01',
+    date: '2016-09',
     homeRegion: ['广东省', '广州市', '海珠区'],
     homeTownRegion: ['广东省', '广州市', '海珠区'],
-    customItem: '全部'
+    customItem: '全部',
+    schoolTypeArray: ['其他','学院/大学', '一本', '211/985/海外院校'],
+    schoolIndex: 0,
   },
 
   onPullDownRefresh: function () {
@@ -27,6 +29,12 @@ Page({
     this.setData({
       homeTownRegion: e.detail.value
     })
+  },
+  bindSchoolTypeChange: function (e) {
+    this.setData({
+      schoolIndex: e.detail.value
+    })
   }
+  
 })
 
