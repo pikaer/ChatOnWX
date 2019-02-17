009523d20e7e654a48873956f52d5fcbd6da5020
易林军 <1441457008@qq.com>
2019-02-18 00:31:33 +0800

    dev

--- pages/userinfo/userinfo.js
+++ pages/userinfo/userinfo.js
@@ -4,7 +4,7 @@ Page({
   data: {
     birthDay: '2016-09-2',
     entranceDate: '2016-09',
-    homeRegion: ['广东省', '广州市', '海珠区'],
+    placeRegion: ['广东省', '广州市', '海珠区'],
     homeTownRegion: ['广东省', '广州市', '海珠区'],
     customItem: '全部',
     schoolTypeArray: ['其他', '学院/大学', '一本', '211/985/海外院校'],
@@ -12,19 +12,19 @@ Page({
     tempUserInfo: {
       "gender": 1,
       "nickName": "Hello",
-      "birthDate": "2016-09-02",
-      "province": "广东省",
-      "city": "广州市",
+      "birthDate": "2016-09-07",
+      "province": "海南省",
+      "city": "三亚市",
       "area": "海珠区",
-      "homeProvince": "广东省",
-      "homeCity": "广州市",
+      "homeProvince": "海南省",
+      "homeCity": "三亚市",
       "homeArea": "海珠区",
-      "schoolName": "上海大学",
-      "entranceDate": "2017-09",
-      "schoolType": 0,
-      "liveState": 0,
-      "mobile": "18721019895",
-      "weChatNo": "xiamu13122028"
+      "schoolName": "上海大学123",
+      "entranceDate": "2017-12",
+      "schoolType": 1,
+      "liveState": 2,
+      "mobile": "18721019895ew",
+      "weChatNo": "xiamu13122028ew"
     }
   },
   onShow: function () {
@@ -51,6 +51,8 @@ Page({
             self.data.tempUserInfo.nickName = content.nickName;
             self.data.tempUserInfo.birthDate = content.birthDate;
             self.data.tempUserInfo.province = content.province;
+            self.data.tempUserInfo.city = content.city;
+            self.data.tempUserInfo.area = content.area;
             self.data.tempUserInfo.homeProvince = content.homeProvince;
             self.data.tempUserInfo.homeCity = content.homeCity;
             self.data.tempUserInfo.homeArea = content.homeArea;
@@ -60,6 +62,16 @@ Page({
             self.data.tempUserInfo.liveState = content.liveState;
             self.data.tempUserInfo.mobile = content.mobile;
             self.data.tempUserInfo.weChatNo = content.weChatNo;
+
+            //所在地下拉框默认值
+            self.data.placeRegion[0] = content.province;
+            self.data.placeRegion[1] = content.city;
+            self.data.placeRegion[2] = content.area;
+
+            //家乡所在地下拉框默认值
+            self.data.homeTownRegion[0] = content.homeProvince;
+            self.data.homeTownRegion[1] = content.homeCity;
+            self.data.homeTownRegion[2] = content.homeArea;
           }
         },
         fail: function (res) { console.error("获取用户信息失败!") }
@@ -74,9 +86,10 @@ Page({
 
   //生日下拉列表框
   bindBirthDayChange: function (e) {
-    this.setData({
-      birthDay: e.detail.value
-    })
+    // let self=this;
+    // this.setData({
+    //   this.data.tempUserInfo.birthDate = e.detail.value
+    // })
   },
 
   //入学时间下拉列表
@@ -87,9 +100,9 @@ Page({
   },
 
   //所在地监听变化
-  bindHomeRegionChange: function (e) {
+  bindPlaceRegionChange: function (e) {
     this.setData({
-      homeRegion: e.detail.value
+      placeRegion: e.detail.value
     })
   },
 
