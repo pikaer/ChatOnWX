a09f6fc634a7b379a403ed358a5667c16a99dac3
Pikaer <1441457008@qq.com>
2019-04-06 18:38:46 +0800

    完善

--- pages/moment/moment.js
+++ pages/moment/moment.js
@@ -1,174 +1,187 @@
 const app = getApp()
 
 Page({
-	data: {
-		isRecommendChecked: true,
-		isNewestChecked: false,
-		isAttentionChecked: false,
-		currentItem: 0,
-		momentList: [],
-		pageIndex: 1,
-		loadHide: true
-	},
+  data: {
+    isRecommendChecked: true,
+    isNewestChecked: false,
+    isAttentionChecked: false,
+    currentItem: 0,
+    momentList: [],
+    pageIndex: 1,
+    loadHide: true
+  },
 
-	//下拉刷新页面数据
-	onPullDownRefresh: function () {
+  //下拉刷新页面数据
+  onPullDownRefresh: function() {
 		this.setData({
-			loadHide: true
+			momentList: [],
+			pageIndex: 1
 		});
-	},
+		this.getMoments(0);
+  },
 
-	stopRefresh:function(loadType) {
-		if (loadType==0){
-			this.setData({
-				loadHide: true
-			});
-		}else{
-			wx.stopPullDownRefresh();
-		}
-		
-	},
+  stopRefresh: function(loadType) {
+    if (loadType == 1) {
+      this.setData({
+        loadHide: true
+      });
+    } else {
+      wx.stopPullDownRefresh();
+    }
+  },
 
   //触底加载更多数据
-	onReachBottom: function (){
-		let page = this.data.pageIndex+1;
-		this.setData({
-			loadHide: false,
-			pageIndex: page
-		});
-		this.getMoments(1);
-	},
+  onReachBottom: function() {
+    let page = this.data.pageIndex + 1;
+    this.setData({
+      loadHide: false,
+      pageIndex: page
+    });
+    this.getMoments(1);
+  },
 
-	onShow: function () {
-		this.getMoments(0);
-	},
+  onShow: function() {
+    this.getMoments(0);
+  },
 
   //置顶
-	toTop: function () {
-		wx.pageScrollTo({
-			scrollTop: 0
-		})
-	},
+  toTop: function() {
+    wx.pageScrollTo({
+      scrollTop: 0
+    })
+  },
 
   //动态详情页面
-	previewMomentDetail: function (e) {
-		let momentId = e.currentTarget.dataset.momentid;
-		wx.navigateTo({
-			url: "../../pages/momentdetail/momentdetail?momentId=" + momentId
-		})
-	},
+  previewMomentDetail: function(e) {
+    let momentId = e.currentTarget.dataset.momentid;
+    wx.navigateTo({
+      url: "../../pages/momentdetail/momentdetail?momentId=" + momentId
+    })
+  },
 
   //发布动态
-	publishMoment: function () {
-		wx.navigateTo({
-			url: '../../pages/publishmoment/publishmoment'
-		})
-	},
-  
-	//跳转个人空间页面
-	toSpace: function (e) {
-		let uId = e.currentTarget.dataset.uId;
-		wx.navigateTo({
-			url: "../../pages/userspace/userspace?uid=" + uId
-		})
-	},
+  publishMoment: function() {
+    wx.navigateTo({
+      url: '../../pages/publishmoment/publishmoment'
+    })
+  },
 
-	//预览图片
-	previewImg: function (e) {
-		let imgContents = e.currentTarget.dataset.imgcontents;
-		let index = e.currentTarget.dataset.index;
-		wx.previewImage({
-			//当前显示图片
-			current: imgContents[index],
-			//所有图片
-			urls: imgContents
-		})
-	},
+  //跳转个人空间页面
+  toSpace: function(e) {
+    let uId = e.currentTarget.dataset.uId;
+    wx.navigateTo({
+      url: "../../pages/userspace/userspace?uid=" + uId
+    })
+  },
 
-	//获取动态
-	getMoments: function (loadType) {
-		var self = this;
-		app.httpPost(
-			'api/Moment/GetMoments', {
-				"UId": app.globalData.apiHeader.UId,
-				"MomentType": this.data.currentItem,
-				"PageIndex": this.data.pageIndex
-			},
-			function (res) {
-				self.setData({
-					momentList: res.momentList,
-					loadHide: true
-				});
-				self.stopRefresh(loadType);
-			},
-			function (res) {
-				console.info("获取数据失败");
-				self.stopRefresh(loadType);
-			})
-	},
+  //预览图片
+  previewImg: function(e) {
+    let imgContents = e.currentTarget.dataset.imgcontents;
+    let index = e.currentTarget.dataset.index;
+    wx.previewImage({
+      //当前显示图片
+      current: imgContents[index],
+      //所有图片
+      urls: imgContents
+    })
+  },
 
-	//Bar切换
-	clickButton: function (e) {
-		switch (e.target.dataset.num) {
-			case "0":
-				this.setData({
-					isRecommendChecked: true,
-					isNewestChecked: false,
-					isAttentionChecked: false,
-					currentItem: 0,
-				})
-				break;
-			case "1":
-				this.setData({
-					isRecommendChecked: false,
-					isNewestChecked: true,
-					isAttentionChecked: false,
-					currentItem: 1
-				})
-				break;
-			case "2":
-				this.setData({
-					isRecommendChecked: false,
-					isNewestChecked: false,
-					isAttentionChecked: true,
-					currentItem: 2
-				})
-				break;
-		}
-		this.getMoments(0);
-	},
+  //获取动态
+  getMoments: function(loadType) {
+    var self = this;
+    let tempMomentList = self.data.momentList;
+    app.httpPost(
+      'api/Moment/GetMoments', {
+        "UId": app.globalData.apiHeader.UId,
+        "MomentType": this.data.currentItem,
+        "PageIndex": this.data.pageIndex
+      },
+      function(res) {
+        if (tempMomentList.length == 0) {
+          tempMomentList = res.momentList
+        } else {
+          if (res.momentList != null && res.momentList.length > 0) {
+            tempMomentList = tempMomentList.concat(res.momentList);
+          }
+        }
+        self.setData({
+          momentList: tempMomentList
+        });
+        self.stopRefresh(loadType);
+      },
+      function(res) {
+        console.info("获取数据失败");
+        self.stopRefresh(loadType);
+      })
+  },
 
-	//点赞或者取消点赞
-	supportChange: function (e) {
-		let key = e.currentTarget.dataset.key;
-		let momentList = this.data.momentList;
-		let supportCount = momentList[key].supportCount;
-		let hasSupport = momentList[key].hasSupport;
-		if (hasSupport) {
-			supportCount--;
-		} else {
-			supportCount++;
-		}
-
-		var count = "momentList[" + key + "].supportCount";
-		var support = "momentList[" + key + "].hasSupport";
+  //Bar切换
+  clickButton: function(e) {
+    switch (e.target.dataset.num) {
+      case "0":
+        this.setData({
+          isRecommendChecked: true,
+          isNewestChecked: false,
+          isAttentionChecked: false,
+          currentItem: 0,
+        })
+        break;
+      case "1":
+        this.setData({
+          isRecommendChecked: false,
+          isNewestChecked: true,
+          isAttentionChecked: false,
+          currentItem: 1
+        })
+        break;
+      case "2":
+        this.setData({
+          isRecommendChecked: false,
+          isNewestChecked: false,
+          isAttentionChecked: true,
+          currentItem: 2
+        })
+        break;
+    }
+		this.toTop();
 		this.setData({
-			[support]: !hasSupport,
-			[count]: supportCount
-		});
+      momentList: [],
+      pageIndex: 1
+    });
+    this.getMoments(0);
+  },
 
-		//更新数据库动态点赞状态
-		app.httpPost(
-			'api/Moment/SupportMoment', {
-				"MomentId": momentList[key].momentId,
-				"UId": app.globalData.apiHeader.UId,
-				"IsSupport": !hasSupport
-			},
-			function (res) {
-				console.info("更新数据库动态点赞状态成功");
-			},
-			function (res) {
-				console.error("更新数据库动态点赞状态失败");
-			})
-	},
+  //点赞或者取消点赞
+  supportChange: function(e) {
+    let key = e.currentTarget.dataset.key;
+    let momentList = this.data.momentList;
+    let supportCount = momentList[key].supportCount;
+    let hasSupport = momentList[key].hasSupport;
+    if (hasSupport) {
+      supportCount--;
+    } else {
+      supportCount++;
+    }
+
+    var count = "momentList[" + key + "].supportCount";
+    var support = "momentList[" + key + "].hasSupport";
+    this.setData({
+      [support]: !hasSupport,
+      [count]: supportCount
+    });
+
+    //更新数据库动态点赞状态
+    app.httpPost(
+      'api/Moment/SupportMoment', {
+        "MomentId": momentList[key].momentId,
+        "UId": app.globalData.apiHeader.UId,
+        "IsSupport": !hasSupport
+      },
+      function(res) {
+        console.info("更新数据库动态点赞状态成功");
+      },
+      function(res) {
+        console.error("更新数据库动态点赞状态失败");
+      })
+  },
 })
